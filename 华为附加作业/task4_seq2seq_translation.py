0b3d8b740eb6ad4695a23f0c08a3370bc255368e
laonuo <144138503+laonuo2004@users.noreply.github.com>
2025-07-05 23:38:46 +0800

    task4

--- 华为附加作业/task4_seq2seq_translation.py
+++ 华为附加作业/task4_seq2seq_translation.py
@@ -91,9 +91,9 @@ def normalizeString(s):
 eng_prefixes = ("i am ", "i m ", "he is", "she is", "they are", "we are", "you are")
 
 def filterPair(p):
-    return len(p[0].split(' ')) < MAX_LENGTH and \\
-        len(p[1].split(' ')) < MAX_LENGTH and \\
-        p[0].startswith(eng_prefixes)
+    return (len(p[0].split(' ')) < MAX_LENGTH and
+            len(p[1].split(' ')) < MAX_LENGTH and
+            p[0].startswith(eng_prefixes))
 
 def filterPairs(pairs):
     return [pair for pair in pairs if filterPair(pair)]
